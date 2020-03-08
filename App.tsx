fe9fc31ba9496272c17a4f222042d1831976a5b6
Simon Heiss <simonheiss87@gmail.com>
2020-03-08 21:15:25 +0100

    endless loop fixed

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import * as Calendar from 'expo-calendar';
+
 
 class Sunset {
     public sunset: Date;
@@ -9,6 +11,15 @@ class Sunset {
     constructor(sunset: Date) {
         this.sunset = sunset;
     }
+}
+
+function formatDate(timeOfSunrise: Date): string {
+    const dtf = new Intl.DateTimeFormat('en', { year: 'numeric', month: '2-digit', day: '2-digit', hour:'2-digit', minute:'2-digit',
+        second:'2-digit'});
+    let dateTimeFormatParts = dtf.formatToParts(timeOfSunrise);
+    const [{ value: mo },,{ value: da },,{ value: ye },,{ value: hr},,{ value: mn}] = dateTimeFormatParts;
+
+    return `${ye}-${mo}-${da}T${hr}:${mn}:00.000Z`;
 }
 
 export default function App() {
@@ -45,12 +56,40 @@ export default function App() {
 
                             setSunRise(new Sunset(timeOfSunrise));
                             setIsLoading(false);
+                            var permission = Permissions.askAsync('calendar');
+                            permission.then(p => {
+                                var cal= Calendar.getCalendarsAsync();
+                                cal.then(c => {
+
+                                    let defaultCalendarAsync = Calendar.getDefaultCalendarAsync();
+                                    defaultCalendarAsync.then(d => {
+                                        Calendar.createEventAsync(d.id, {
+                                            startDate: timeOfSunrise,
+                                            endDate: timeOfSunrise,
+                                            title: "Cockk Time Is on!",
+                                            timeZone: "GMT-7",
+                                            alarms: [ { absoluteDate: formatDate(timeOfSunrise) } ]
+                                        })
+                                            .then( event => {
+                                                console.log('success',event);
+                                            })
+                                            .catch( error => {
+                                                console.log('failure',error);
+                                            });
+                                    })
+
+                                })
+
+
+                            })
+
+
                         })
                         .catch(error => console.log(error));
                 });
             }
         });
-    }, [sunrise]);
+    }, []);
 
     let content = isLoading
         ? <Text>Loading...</Text>
