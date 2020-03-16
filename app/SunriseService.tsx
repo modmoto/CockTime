f7267166adf1e4c2ef17020d69efb69978d378e3
Simon Heiss <simonheiss87@gmail.com>
2020-03-16 17:45:03 +0100

    fixi fucki

--- app/SunriseService.tsx
+++ app/SunriseService.tsx
@@ -5,14 +5,17 @@ interface ILocation {
     longitude: number;
 }
 
-export default function getSunrises(location: ILocation) {
-    let date = new Date();
-    let nextDate = new Date();
+export default function getSunrises(location: ILocation, currentDate: Date = null) {
+
+    let date = currentDate ? currentDate : new Date();
+    let nextDate = new Date(currentDate);
+    let day = nextDate.getDate();
+    nextDate.setDate(day + 1);
     if (date.getHours() > 4) {
-        date.setDate(date.getDay() + 1);
-        nextDate.setDate(nextDate.getDay() + 2);
+        date.setDate(date.getDate() + 1);
+        nextDate.setDate(nextDate.getDate() + 2);
     }
     const todays = SunCalc.getTimes(date, location.latitude, location.longitude);
     const nextSunrise = SunCalc.getTimes(nextDate, location.latitude, location.longitude);
-    return {todays, nextSunrise};
+    return { todays, nextSunrise };
 }
