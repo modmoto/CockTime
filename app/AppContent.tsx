f0c7cb95303668104ac409542c239d31f7d4ea0a
Simon Heiss <simonheiss87@gmail.com>
2020-03-17 09:35:23 +0100

    fing go

--- app/AppContent.tsx
+++ app/AppContent.tsx
@@ -30,8 +30,13 @@ export default function AppContent ({navigation}) {
                     const finalDayOfEaseTime = new Date(settings.easeTimeStartedAt.getTime() + 86400000 * settings.easeTimeDuration );
                     const {todays: sunriseOnFinalDay } = getSunrises(location, finalDayOfEaseTime);
                     const {todays: sunriseOnStartDay } = getSunrises(location, settings.easeTimeStartedAt);
-                    const interval = (sunriseOnFinalDay.sunrise.getTime() - sunriseOnStartDay.sunrise.getTime()) / settings.easeTimeDuration;
+                    let time = sunriseOnFinalDay.sunrise;
+                    let time1 = sunriseOnStartDay.sunrise;
 
+                    time.setFullYear(0, 0, 0);
+                    time1.setFullYear(0, 0, 0);
+                    let number = time.getTime() - time1.getTime();
+                    const interval = number / settings.easeTimeDuration;
                     setSunRise(new TimesOfTheDay(todays.sunrise, nextSunrise.sunrise, interval));
                 } else {
                     setSunRise(new TimesOfTheDay(todays.sunrise, nextSunrise.sunrise, 0));
@@ -59,9 +64,9 @@ export default function AppContent ({navigation}) {
 
     let content = sunrise ? (
         <>
-            <Text style={styles.watchText}>{sunrise.cocktime.toLocaleTimeString().slice(0, -6)} CT</Text>
+            <Text style={styles.watchText}>{sunrise.cocktime.toLocaleTimeString().slice(0, -3)} CT</Text>
             {
-                <Text style={styles.watchSubText}>Set Alarm to {sunrise.sunrise.toLocaleTimeString().slice(0, -6)} {sunrise.easeTimSunrise ? '(' + sunrise.easeTimSunrise.toLocaleTimeString().slice(0, -6) + ')' : null}</Text>
+                <Text style={styles.watchSubText}>Set Alarm to {sunrise.sunrise.toLocaleTimeString().slice(0, -3)} {sunrise.easeTimSunrise ? '(' + sunrise.easeTimSunrise.toLocaleTimeString().slice(0, -3) + ')' : null}</Text>
             }
         </>
     ) : null;
@@ -69,15 +74,15 @@ export default function AppContent ({navigation}) {
         ? <View style={styles.timeContainer}>
             <View style={styles.buttonUp}>
                 <FontAwesomeIcon style={styles.watchSubText} size={screen.width/10} icon={ faSun } />
-                <Text style={styles.textColorAccent}>-{sunrise.timeToNextGetingUp.toLocaleTimeString().slice(0, - 6)}</Text>
+                <Text style={styles.textColorAccent}>-{sunrise.timeToNextGetingUp.toLocaleTimeString().slice(0, - 3)}</Text>
             </View>
             <View style={styles.buttonDown}>
                 <FontAwesomeIcon style={styles.watchSubText} size={screen.width/10} icon={ faUtensils } />
-                <Text style={styles.textColorAccent}>-{sunrise.timeToNextLunch.toLocaleTimeString().slice(0, - 6)}</Text>
+                <Text style={styles.textColorAccent}>-{sunrise.timeToNextLunch.toLocaleTimeString().slice(0, - 3)}</Text>
             </View>
             <View style={styles.buttonUp}>
                 <FontAwesomeIcon style={styles.watchSubText} size={screen.width/10} icon={ faMoon } />
-                <Text style={styles.textColorAccent}>-{sunrise.timeToBed.toLocaleTimeString().slice(0, - 6)}</Text>
+                <Text style={styles.textColorAccent}>-{sunrise.timeToBed.toLocaleTimeString().slice(0, - 3)}</Text>
             </View>
         </View>
         : null;
