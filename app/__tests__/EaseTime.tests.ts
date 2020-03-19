cd6b4cc41f72f331b6ae5043c8e53cbdae0f85c2
Simon Heiss <simonheiss87@gmail.com>
2020-03-19 09:25:30 +0100

    more tests

--- app/__tests__/EaseTime.tests.ts
+++ app/__tests__/EaseTime.tests.ts
@@ -41,3 +41,13 @@ test('EaseTime 3 day later', () => {
     expect(et.hours()).toBe(1);
     expect(et.minutes()).toBe(24);
 });
+
+test('EaseTime on the first day, 20 days ease time, sun goes up on 5:54, so 154 minutes off', () => {
+    const ctSettings = new CTSettings();
+    ctSettings.normalGetUpTime = moment({hours:8, minutes:30});
+    ctSettings.easeTimeStartedAt = moment().date(18).month(3).minutes(4);
+    ctSettings.easeTimeDuration = 20;
+    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(18).month(3).minutes(5));
+    expect(et.hours()).toBe(2);
+    expect(et.minutes()).toBe(28);
+});
