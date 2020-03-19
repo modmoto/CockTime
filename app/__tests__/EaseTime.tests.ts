5533ba6766a2b5529ec54cf0a4dd63d14bb3ac17
Simon Heiss <simonheiss87@gmail.com>
2020-03-19 09:19:31 +0100

    tests now right?

--- app/__tests__/EaseTime.tests.ts
+++ app/__tests__/EaseTime.tests.ts
@@ -5,39 +5,39 @@ import {CTSettings} from "../CTSettings";
 test('EaseTime on the first day, sun goes up at 6:10', () => {
     const ctSettings = new CTSettings();
     ctSettings.normalGetUpTime = moment({hours:8, minutes:30});
-    ctSettings.easeTimeStartedAt = moment().date(18).month(3);
+    ctSettings.easeTimeStartedAt = moment().date(18).month(3).minutes(4);
     ctSettings.easeTimeDuration = 10;
-    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(18).month(3));
+    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(18).month(3).minutes(5));
     expect(et.hours()).toBe(2);
-    expect(et.minutes()).toBe(18);
+    expect(et.minutes()).toBe(6);
 });
 
 test('EaseTime 1 day later', () => {
     const ctSettings = new CTSettings();
     ctSettings.normalGetUpTime = moment({hours:8, minutes:30});
-    ctSettings.easeTimeStartedAt = moment().date(18).month(3);
+    ctSettings.easeTimeStartedAt = moment().date(18).month(3).minutes(4);
     ctSettings.easeTimeDuration = 10;
-    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(19).month(3));
-    expect(et.hours()).toBe(2);
-    expect(et.minutes()).toBe(4);
+    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(19).month(3).minutes(5));
+    expect(et.hours()).toBe(1);
+    expect(et.minutes()).toBe(52);
 });
 
 test('EaseTime 2 day later', () => {
     const ctSettings = new CTSettings();
     ctSettings.normalGetUpTime = moment({hours:8, minutes:30});
-    ctSettings.easeTimeStartedAt = moment().date(18).month(3);
+    ctSettings.easeTimeStartedAt = moment().date(18).month(3).minutes(4);
     ctSettings.easeTimeDuration = 10;
-    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(20).month(3));
+    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(20).month(3).minutes(5));
     expect(et.hours()).toBe(1);
-    expect(et.minutes()).toBe(50);
+    expect(et.minutes()).toBe(38);
 });
 
 test('EaseTime 3 day later', () => {
     const ctSettings = new CTSettings();
     ctSettings.normalGetUpTime = moment({hours:8, minutes:30});
-    ctSettings.easeTimeStartedAt = moment().date(18).month(3);
+    ctSettings.easeTimeStartedAt = moment().date(18).month(3).minutes(4);
     ctSettings.easeTimeDuration = 10;
-    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(21).month(3));
+    const et = calculateEaseTime(ctSettings, {latitude: 49.0040869, longitude: 8.5261946}, moment().date(21).month(3).minutes(5));
     expect(et.hours()).toBe(1);
-    expect(et.minutes()).toBe(36);
+    expect(et.minutes()).toBe(24);
 });
